47444aa7af740521869aaa03e16151ba312e3032
mlazzell17@gmail.com <MicheSi>
2019-10-17 13:23:26 -0700

    second commit

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -36,13 +36,28 @@ class Student extends Person {
         return favSubjects;
     }
     PRAssignment(subject){
-        return `${student.name} has submitted a PR for ${subject}`;
+        return `${this.name} has submitted a PR for ${subject}`;
     }
-    sprintChallenge(){
-        return `${student.name} has begun sprint challenge on ${subject}`;
+    sprintChallenge(subject){
+        return `${this.name} has begun sprint challenge on ${subject}`;
     }
 }
 
+class ProjectManager extends Instructor {
+    constructor (pmAttr){
+        super(pmAttr);
+        this.gradClassName = pmAttr.gradClassName,
+        this.favInstructor = pmAttr.favInstructor
+    }
+    standUp(channel){
+        return `${this.name} announces to ${channel}, @channel standy times!`;
+    }
+    debugsCode(student, subject){
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
+    }
+}
+
+// Person objects
 const Lilo = new Person({
     name: 'Lilo',
     age: 26,
@@ -61,6 +76,7 @@ const Ariel = new Person ({
     location: 'Under the Sea'
 });
 
+// Instructor objects
 const Goku = new Instructor({
     name: 'Goku',
     age: '36',
@@ -71,9 +87,68 @@ const Goku = new Instructor({
 });
 
 const Piccolo = new Instructor({
+    name: 'Piccolo',
+    age: '40',
+    location: 'Namek',
+    specialty: 'special beam cannon',
+    favLanguage: 'Java',
+    catchPhrase: "Fool!"
+});
 
-})
+const MasterRoshi = new Instructor({
+    name: 'Master Roshi',
+    age: '100',
+    location: 'Kame House',
+    specialty: 'martial arts',
+    favLanguage: 'Ruby',
+    catchPhrase: "Yaaahhh!"
+});
 
+// Student Objects
+const Gohan = new Student({
+    name: 'Gohan',
+    age: '18',
+    location: 'Mount Paozu',
+    previousBackground: 'martial arts training',
+    className: 'Web26',
+    favSubjects: ['HTML', 'CSS', 'math']
+});
+
+const Michelle = new Student({
+    name: 'Michelle',
+    age: '36',
+    location: 'Arizona',
+    previousBackground: 'administrative',
+    className: 'Web25',
+    favSubjects: ['HTML', 'CSS', 'Psychology']
+});
+
+const Oli = new Student({
+    name: 'Oli',
+    age: '38',
+    location: 'Scotland',
+    previousBackground: 'finance',
+    className: 'Web25',
+    favSubjects: ['Javascript', 'Ruby', 'Python']
+});
+
+// PM Objects
+const King Kai = new ProjectManager({
+    name: 'King Kai',
+    age: '125',
+    location: 'his own planet',
+    specialty: 'spirit bomb',
+    favLanguage: 'Ruby',
+    catchPhrase: "Jokes on you!",
+    gradClassName: 'Web25',
+    favInstructor: 'Goku'
+});
+
+
+
+// Console logs to run tests
 console.log(Mulan.speak());
 console.log(Goku.demo('physics'));
-console.log(Goku.grade('Gohan', 'math'));
+console.log(Piccolo.grade(Gohan, 'math'));
+console.log(Michelle.PRAssignment('JSII'));
+console.log(Oli.sprintChallenge('CSS'));
